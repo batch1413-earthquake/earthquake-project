6fdc800c013a5c55b1dec84c02769e0a25e3226a
Amin Messaoudi <amin_messaoudi@yahoo.com>
2023-11-09 09:27:08 +0000

    update entrypoint no need of poetry

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-poetry run airflow db upgrade
+airflow db upgrade
 
-poetry run airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
+airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
 
-poetry run airflow webserver
+airflow webserver
